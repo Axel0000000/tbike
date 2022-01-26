ab67962c07a8ddd9c45b4128209e8b4f7e93f69b
Axel0000000 <ostinskyi@gmail.com>
2022-01-26 02:13:02 +0200

    added variants

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+ 
+   $(".variants__star").rateYo({
+      starWidth: "14px",
+      normalFill: "#d6d6d6",
+      ratedFill: "#FBC400",
+      readOnly: true,
+      fullStar: true,
+      spacing: "8px",
+      starSvg:'<svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 0L9.31235 3.81733L13.6574 4.83688L10.7415 8.21567L11.1145 12.6631L7 10.934L2.8855 12.6631L3.25854 8.21567L0.342604 4.83688L4.68765 3.81733L7 0Z"/></svg>',
+   });
+
+});
+
